3b3a7dfe4a4647abe5e4633cbc6a408e5a862592
ganeshshah <shahganesh777@gmail.com>
2022-05-16 23:35:47 +0530

    Home Page structure completed

---
+++ src/views/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Container from "@mui/material/Container";
+import NavList from "../uicomponents/NavList";
+import Particlejsmain from "../uicomponents/particlejs/Particlejsmain";
+import "../css/App.css";
+import Grid from "@mui/material/Grid";
+
+function About() {
+  return (
+    <Grid container spacing={0}>
+      <Particlejsmain />
+      <Grid item xs={2}>
+        <Container className="nav-parent">
+          <NavList />
+        </Container>
+      </Grid>
+      <Grid item xs={10}>
+        <Container id="main" className="home-container">
+          <div>About</div>
+        </Container>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default About
